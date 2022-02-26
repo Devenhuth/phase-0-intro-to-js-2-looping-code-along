5820a1e82117985439c298d26888bb470d233b3d
Deven <devenzachuth@gmail.com>
2022-02-26 14:00:24 -0600

    commit tree a6e8d6fd16430a8e5d386b570f49973283b75de8

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const people = ["Guadalupe", "Ollie", "Aki"]
+const type = ["surprise"]
+
+function writeCards() {
+    let empty = []
+    for (let i = 0; i < people.length; i++) {
+        empty.push(`Thank you, ${people[i]}, for the wonderful ${type} gift!`);
+    }
+
+    return empty;
+}
+
+function countDown() {
+    let n = 10
+    while (n >= 0) {
+        console.log(n)
+        n--
+    }
+    return n;
+}
